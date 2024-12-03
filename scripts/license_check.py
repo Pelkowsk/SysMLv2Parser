883902a37427581cc91a08109123461d00729632
Pelkowsk <pelkowsk@rhrk.uni-kl.de>
2024-12-03 21:01:52 +0100

    Endspurt 03.12 ( Ok läuft alles, jetzt wirds hübsch gemacht )

--- scripts/license_check.py
+++ scripts/license_check.py
@@ -1,9 +1,45 @@
 import os
 import json
 
-# Konfigurationsparameter
-PROHIBITED_LICENSES = ['prohibited_license_1', 'prohibited_license_2']  # Ersetzen Sie diese Platzhalter durch die tatsächlichen Lizenzschlüssel
-REQUIRED_LICENSE_HEADER = """[Ihr Lizenz-Header hier]"""  # Ersetzen Sie diesen Platzhalter durch den tatsächlichen Lizenz-Header
+#konfiguration dependend on projects license:
+
+PROHIBITED_LICENSES = ["afl-3.0", "apache-2.0", "artistic-1.0", "bittorrent-1.0", "bittorrent-1.1",
+                       "cc-by-nc-1.0", "cc-by-nc-2.0", "cc-by-nc-2.5", "cc-by-nc-3.0", "cc-by-nc-4.0",
+                       "cc-by-nc-nd-1.0", "cc-by-nc-nd-2.0", "cc-by-nc-nd-2.5", "cc-by-nc-nd-3.0",
+                       "cc-by-nc-nd-4.0", "cc-by-nc-sa-1.0", "cc-by-nc-sa-2.0", "cc-by-nc-sa-2.5",
+                       "cc-by-nc-sa-3.0", "cc-by-nc-sa-4.0"]  # here list of prohibited licenses
+REQUIRED_LICENSE_HEADER = """/*****************************************************************************
+ * SysML 2 Pilot Implementation
+ * Copyright (c) 2018-2024 Model Driven Solutions, Inc.
+ * Copyright (c) 2018 IncQuery Labs Ltd.
+ * Copyright (c) 2019 Maplesoft (Waterloo Maple, Inc.)
+ * Copyright (c) 2019 Mgnite Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @license LGPL-3.0-or-later <http://spdx.org/licenses/LGPL-3.0-or-later>
+ *
+ * Contributors:
+ *  Ed Seidewitz, MDS
+ *  Zoltan Kiss, IncQuery
+ *  Balazs Grill, IncQuery
+ *  Hisashi Miyashita, Maplesoft/Mgnite
+ *
+ *****************************************************************************/""" # here project license header
+
+# end konfiguration
+
 
 def check_licenses_and_headers():
     # Pfad zur ScanCode-Ergebnisdatei
